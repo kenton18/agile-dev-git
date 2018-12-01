7bc236df72f44aaf6f965d30be3a898f5198099c
ckmpk2004 <44965887+ckmpk2004@users.noreply.github.com>
2018-12-01 17:38:24 +0800

    Unit test completed
    I have completed the MongoDB CRUD function's unit test.
    Please review and test. Thanks!

---
+++ test_delete.js
@@ -0,0 +1,45 @@
+//Describe tests
+var mocha = require('mocha');
+var assert = require('assert');
+var FoodModel = require('../routes/index.js');
+var food;
+
+    // Create a food that ready for delete
+    before(function(){
+         food = new FoodModel ({
+            Food_Name: 'Orange',
+            Buy_Date: 'a',
+            Expired_date:'b',
+            best_before_date: 'c',
+            Food_type: 'd'
+        });
+        food.save();
+    });
+
+describe('Delete Record', function() {
+    this.timeout(0);
+    it('Deletes one record from MongoDB', function (done) {
+       FoodModel.findOneAndDelete({
+            Food_Name: 'Orangeee',
+            Buy_Date: 'a',
+            Expired_date:'b',
+            best_before_date: 'c',
+            Food_type: 'd'
+        }, function(result){
+
+            FoodModel.find({
+                Food_Name: 'Orange',
+                Buy_Date: 'a',
+                Expired_date:'b',
+                best_before_date: 'c',
+                Food_type: 'd'
+            }, function(err, result){
+                
+                if(result.length) {
+                    assert(false);
+                }
+            })
+        })
+    done();
+    });
+});
