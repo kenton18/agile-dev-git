f6cf785f02667c4c696421d70d58fb2d610e32f1
kenton18 <31765235+kenton18@users.noreply.github.com>
2018-12-02 21:36:14 +0800

    fix the bug

--- Test/test_delete.js
+++ Test/test_delete.js
@@ -20,7 +20,7 @@ describe('Delete Record', function() {
     this.timeout(0);
     it('Deletes one record from MongoDB', function (done) {
        FoodModel.findOneAndDelete({
-            Food_Name: 'Orangeee',
+            Food_Name: 'Orange',
             Buy_Date: 'a',
             Expired_date:'b',
             best_before_date: 'c',
@@ -34,12 +34,16 @@ describe('Delete Record', function() {
                 best_before_date: 'c',
                 Food_type: 'd'
             }, function(err, result){
-                
+                console.log(result)
+                console.log(typeof(result))
                 if(result.length) {
                     assert(false);
+                    
                 }
+                done();
+                
             })
         })
-    done();
+        
     });
 });
